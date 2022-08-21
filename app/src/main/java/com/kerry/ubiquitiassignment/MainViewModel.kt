5459b273e8d4fec5a06a275fad5cb088e3605b53
Kerry <kerrychen0815@gmail.com>
2022-08-21 14:38:48 +0800

    inject repository into viewModel for api call

--- app/src/main/java/com/kerry/ubiquitiassignment/MainViewModel.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/MainViewModel.kt
@@ -1,14 +1,43 @@
 package com.kerry.ubiquitiassignment
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.kerry.ubiquitiassignment.network.ApiService
+import androidx.lifecycle.viewModelScope
+import com.kerry.ubiquitiassignment.model.Record
+import com.kerry.ubiquitiassignment.repository.MyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val apiService: ApiService
+    private val repo: MyRepository
 ) : ViewModel() {
 
+    private val _goodStatusRecords = MutableLiveData<List<Record?>>()
+    val goodStatusRecords: LiveData<List<Record?>> get() = _goodStatusRecords
+
+    private val _badStatusRecords = MutableLiveData<List<Record?>>()
+    val badStatusRecords: LiveData<List<Record?>> get() = _badStatusRecords
+
+    private val _enableErrorAlert = MutableLiveData<Boolean>()
+    val enableErrorAlert: LiveData<Boolean> get() = _enableErrorAlert
+
+    fun fetchRecordList() = viewModelScope.launch {
+        when (val result = repo.getAirDataResult(limit = 1000, apiKey = BuildConfig.API_KEY)) {
+            is ApiResult.Success -> {
+                val list: List<Record?> = result.data
+
+
+                _enableErrorAlert.value = false
+            }
+            else -> {
+                _enableErrorAlert.value = true
+            }
+        }
+
+    }
 
 }
