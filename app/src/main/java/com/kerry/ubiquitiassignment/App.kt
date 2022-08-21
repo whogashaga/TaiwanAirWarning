4355fc57a1111090a3b004377dc7bd6ef41a2a00
Kerry <kerrychen0815@gmail.com>
2022-08-21 14:20:02 +0800

    register MyApp in AndroidManifest

--- app/src/main/java/com/kerry/ubiquitiassignment/App.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/App.kt
@@ -3,6 +3,10 @@ package com.kerry.ubiquitiassignment
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+
 class MyApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
