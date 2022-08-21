f72502223c1cdfc91894443b5b9d102a747a20ea
Kerry <kerrychen0815@gmail.com>
2022-08-21 11:52:24 +0800

    set up toolbar

--- app/src/main/java/com/kerry/ubiquitiassignment/MainActivity.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/MainActivity.kt
@@ -2,15 +2,18 @@ package com.kerry.ubiquitiassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.kerry.ubiquitiassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
     }
 }
