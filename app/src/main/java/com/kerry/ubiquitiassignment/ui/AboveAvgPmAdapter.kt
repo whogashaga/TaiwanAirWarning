0570894a47c3c9582f58f1b91f9ee97d8be55434
Kerry <kerrychen0815@gmail.com>
2022-08-21 17:33:51 +0800

    remove testing code

--- app/src/main/java/com/kerry/ubiquitiassignment/ui/AboveAvgPmAdapter.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/ui/AboveAvgPmAdapter.kt
@@ -53,15 +53,6 @@ class AboveAvgPmViewHolder(
 
         if (record?.isStatusGood == true) {
             binding.ivArrow.gone()
-            if (position % 3 == 0) {
-                binding.ivArrow.visible()
-                binding.root.setOnClickListener {
-                    if (record != null) {
-                        onArrowClick.invoke(record)
-                    }
-                }
-                return
-            }
             binding.root.setOnClickListener {  }
         } else {
             binding.ivArrow.visible()
