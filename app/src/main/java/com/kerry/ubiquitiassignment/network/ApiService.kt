61911d3b3717985edf23edb18a686e07846e4b06
Kerry <kerrychen0815@gmail.com>
2022-08-21 13:00:38 +0800

    extract api key

--- app/src/main/java/com/kerry/ubiquitiassignment/network/ApiService.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/network/ApiService.kt
@@ -1,20 +1,19 @@
 package com.kerry.ubiquitiassignment.network
 
 import com.kerry.ubiquitiassignment.model.AirDataResult
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("aqx_p_432?limit=1000&api_key=58f8c0e0-d154-4d6d-b062-54001d8fa192&sort=ImportDate%20desc&format=json")
-    fun getAirData(
+    @GET("aqx_p_432")
+    fun getAirDataDeferred(
         @Query("limit") limit: String,
         @Query("api_key") apiKey: String,
         @Query("sort") sort: String,
         @Query("format") format: String
-    ): Call<AirDataResult>
-
-// https://data.epa.gov.tw/api/v2/aqx_p_432?limit=1000&api_key=58f8c0e0-d154-4d6d-b062-54001d8fa192&sort=ImportDate%20desc&format=json
+    ): Deferred<AirDataResult>
 
 }
