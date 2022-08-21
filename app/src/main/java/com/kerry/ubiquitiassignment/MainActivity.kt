5d337b794b8522cfc9bc8c89649040bed271d5bf
Kerry <kerrychen0815@gmail.com>
2022-08-21 14:39:03 +0800

    test api call

--- app/src/main/java/com/kerry/ubiquitiassignment/MainActivity.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.kerry.ubiquitiassignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.kerry.ubiquitiassignment.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        viewModel.fetchRecordList()
+
+        viewModel.goodStatusRecords.observe(this) {
+            AlertDialog.Builder(this)
+                .setMessage("size: ${it.size}")
+                .show()
+        }
 
     }
 }
