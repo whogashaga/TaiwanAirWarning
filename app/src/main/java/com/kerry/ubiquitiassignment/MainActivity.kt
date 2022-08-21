2aa4d2d12645b19e6309c7b5a6f49df8c8e7a078
Kerry <kerrychen0815@gmail.com>
2022-08-21 11:40:14 +0800

    implement ViewBinding

--- app/src/main/java/com/kerry/ubiquitiassignment/MainActivity.kt
+++ app/src/main/java/com/kerry/ubiquitiassignment/MainActivity.kt
@@ -2,10 +2,15 @@ package com.kerry.ubiquitiassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kerry.ubiquitiassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
